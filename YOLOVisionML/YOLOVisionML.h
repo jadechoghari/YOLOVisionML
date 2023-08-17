8ce0ef517d43f0d02efbc2882e847713386edb5b
Jade Choghari <chogharijade@icloud.com>
2023-08-17 22:24:58 +0300

    Initial project setup

---
+++ YOLOVisionML/YOLOVisionML.h
@@ -0,0 +1,18 @@
+//
+//  YOLOVisionML.h
+//  YOLOVisionML
+//
+//  Created by Jade Choghari on 17/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YOLOVisionML.
+FOUNDATION_EXPORT double YOLOVisionMLVersionNumber;
+
+//! Project version string for YOLOVisionML.
+FOUNDATION_EXPORT const unsigned char YOLOVisionMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YOLOVisionML/PublicHeader.h>
+
+
